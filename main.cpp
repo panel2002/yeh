3113180c6f32b02d34cad99790af45085409d557
panel2002 <84904389+panel2002@users.noreply.github.com>
2021-05-27 16:12:22 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int *myPointer = new int(5);
+	//myPointer[1] = 6;
+	cout << *myPointer << endl;
+}
